933fb2784b23d07b85ae7ead14541f64e67820ba
YoungJoo.Kim <vozlt@vozlt.com>
2015-04-09 22:17:53 +0900

    Compatibility: changed the position of nginx.h to avoid compile error on windows at v1.7.12

--- src/ngx_http_vhost_traffic_status_module.c
+++ src/ngx_http_vhost_traffic_status_module.c
@@ -10,10 +10,10 @@
  */
 
 
-#include <nginx.h>
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
+#include <nginx.h>
 
 #include "ngx_http_vhost_traffic_status_module_html.h"
 
