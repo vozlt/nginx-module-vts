203bc5bf2d9a2cbf30e7b84313011d8a0adea191
YoungJoo.Kim <vozltx@gmail.com>
2021-01-27 10:02:43 +0000

    Bugfix: rollback to 549cc4d

--- src/ngx_http_vhost_traffic_status_module.h
+++ src/ngx_http_vhost_traffic_status_module.h
@@ -16,7 +16,7 @@
 #include "ngx_http_vhost_traffic_status_string.h"
 #include "ngx_http_vhost_traffic_status_node.h"
 
-#define NGX_HTTP_VTS_MODULE_VERSION  "0.1.19.dev.c16aa0e"
+#define NGX_HTTP_VTS_MODULE_VERSION  "0.1.19.dev.549cc4d"
 
 #define NGX_HTTP_VHOST_TRAFFIC_STATUS_UPSTREAM_NO          0
 #define NGX_HTTP_VHOST_TRAFFIC_STATUS_UPSTREAM_UA          1
